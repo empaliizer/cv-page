fdd3d231be6f655e579d5a30899368362b2fd984
Emelie <90389341+empaliizer@users.noreply.github.com>
2022-10-25 10:35:11 +0200

    add to jsonfile
    Remove the elements in HTML and added array with objetcs in json file

--- js/cv.js
+++ js/cv.js
@@ -1,57 +1,56 @@
-fetch('../data/cv.json')
-    .then(res => res.json())
-    .then(obj => {
-        /* 
-            Fetch hämtar data från en url
-            Första then gör om datan till json, filen kommer in som sträng
-            Andra then händer när datan är konverterad till JSON som javascript kan arbeta med.
-            Objektet från filen sparas i namnet "json", denna variabel kan man döpa till vad man vill. 
+const createCard = (obj) => {
+  const cvCard = document.createElement("div");
+  const year = document.createElement("div");
+  const deco = document.createElement("div");
+  const dot = document.createElement("div");
+  const verLine = document.createElement("div");
+  const infoText = document.createElement("div");
+  const heading = document.createElement("h4");
+  const info = document.createElement("p");
 
-            <div class="cv-card">
-                <div class="year">
-                    <p>2022-2024</p>
-                </div >
-                <div class="decoration">
-                    <div class="dot"></div>
-                    <div class="ver-line"></div>
-                </div>
-                <div class="info-text">
-                    <h4 class="mb-xs">Chas Academy</h4>
-                    <p class="mb-s">Went two years in school to get a education in UX/UI with frontend knowledge.</p>
-                </div>
-            </div>
+  cvCard.classList.add("cv-card");
+  year.classList.add("year");
+  deco.classList.add("decoration");
+  dot.classList.add("dot");
+  verLine.classList.add("ver-line");
+  infoText.classList.add("info-text");
+  heading.classList.add("mb-xs");
+  info.classList.add("mb-s");
+
+  year.innerHTML = `<p>${obj.year}</p>`;
+  heading.innerHTML = obj.heading;
+  info.innerHTML = obj.description;
+
+  cvCard.appendChild(year);
+  cvCard.appendChild(deco);
+  deco.appendChild(dot);
+  deco.appendChild(verLine);
+  cvCard.appendChild(infoText);
+  infoText.appendChild(heading);
+  infoText.appendChild(info);
+
+    return cvCard;
+};
 
 
-        */
-        console.log(obj);
+fetch("../data/cv.json")
+  .then((res) => res.json())
+  .then((obj) => {
 
-        const cardContainer = document.querySelector('#education');
+    const eduContainer = document.querySelector("#education");
+    const expContainer = document.querySelector("#experience");
 
-        for(let edu of obj.education) {
-            const cvCard = document.createElement('div');
-            const year = document.createElement('div');
-            const deco = document.createElement('div');
-            const dot = document.createElement('div');
-            const verLine = document.createElement('div');
-            const infoText = document.createElement('div');
+    for (let edu of obj.education) {
+        const cvCard = createCard(edu);
+      /* Lägg till element i htmlen */
+      eduContainer.appendChild(cvCard);
+    }
 
-            cvCard.classList.add('cv-card');
-            year.classList.add('year');
-            deco.classList.add('decoration');
-            dot.classList.add('dot');
-            verLine.classList.add('ver-line');
-            infoText.classList.add('info-text');
+    for (let exp of obj.experience) {
+        const cvCard = createCard(exp);
 
-            year.innerHTML = `<p>${edu.year}</p>`;
+        expContainer.appendChild(cvCard);
+    }
 
-            cvCard.appendChild(year);
-            cvCard.appendChild(deco);
-            deco.appendChild(dot);
-            deco.appendChild(verLine);
-            cvCard.appendChild(infoText);
 
-            /* Lägg till element i htmlen */
-            cardContainer.appendChild(cvCard);
-        }
-        
-    });
+  });
